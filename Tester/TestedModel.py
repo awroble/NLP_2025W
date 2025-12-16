463008a8af82f323baec4f4d54a0649cb63d586b
shamajn <piotr.rowicki02@gmail.com>
2025-12-16 19:38:07 +0100

    trained bert model, extended testing suite. PoC tests done

--- Tester/TestedModel.py
+++ Tester/TestedModel.py
@@ -1,21 +1,40 @@
 
 from typing import Sequence
 import numpy as np
+import torch
+from numpy.typing import NDArray
 class TestedModel:
-    def predict(self,texts:Sequence[str])->float:
+    def predict(self,texts:NDArray[str])->float:
         pass
     def get_name(self)->str:
         pass
 
 
 class ClassicalModel(TestedModel):
+    def __init__(self,model,name):
+        self.model=model
+        self.name=name
+
+    def predict(self,texts:NDArray[str])->Sequence[int]:
+        return self.model.predict(texts).astype(int)
+    def get_name(self)->str:
+        return self.name
+
+class BertModel(TestedModel):
+
     def __init__(self,model,tokenizer,name):
         self.model=model
         self.tokenizer=tokenizer
         self.name=name
 
-    def predict(self,texts:Sequence[str])->Sequence[int]:
-        vectorized=self.tokenizer.transform(texts)
-        return self.model.predict(vectorized)
+    def predict(self,texts:NDArray[str])->Sequence[int]:
+        t_vec=self.tokenizer(texts.tolist(),
+                            return_tensors = "pt",
+                            truncation = True,
+                            padding = True,
+                            )
+        with torch.no_grad():
+            result=self.model(**t_vec)
+        return torch.argmax(result.logits, dim=1).detach().numpy()
     def get_name(self)->str:
         return self.name
--- Tester/TestingSuite.py
+++ Tester/TestingSuite.py
@@ -1,11 +1,69 @@
 from typing import List,Dict,Sequence
 from  TestedModel import TestedModel
+from numpy.typing import NDArray
+import numpy as np
+import sklearn.metrics as metrics
+import time
+
+
+def evaluate_metric(y_pred,y_true,metric):
+    match metric:
+        case "f1_score":
+            return metrics.f1_score(y_true,y_pred)
+        case "accuracy":
+            return metrics.accuracy_score(y_true,y_pred)
+        case _:
+            raise ValueError("invalid metric")
 
 class ModelsTestingSuite:
-    def __init__(self):
+
+
+    def __init__(self,X:NDArray,y:NDArray):
         self.models:List[TestedModel] = []
+        self.X:NDArray=X
+        self.y:NDArray=y
+
+    def change_ds(self,X:NDArray,y:NDArray):
+        self.X:NDArray=X
+        self.y:NDArray=y
+    def _init_results(self,batch_nr:int):
+        results = {}
+        for model in self.models:
+            results[model.get_name()]=np.zeros(batch_nr)
+        return results
+
+    def _init_splits(self,batch_nr:int,seed:int):
+        N=self.X.shape[0]
+        idx=np.arange(N)
+        np.random.seed(seed)
+        np.random.shuffle(idx)
+        return np.array_split(idx,batch_nr)
 
     def add(self, model: TestedModel):
         self.models.append(model)
-    def evaluate(self, metric:str) -> Dict[str, Sequence[float]]:
-        return None
+
+    def evaluate(self, metric:str,batch_nr:int,seed:int=42) -> Dict[str,np.ndarray]:
+        results = self._init_results(batch_nr)
+        splits = self._init_splits(batch_nr,seed)
+        for i,split in enumerate(splits):
+            X_b=self.X[split]
+            y_b=self.y[split]
+            for model in self.models:
+                y_pred=model.predict(X_b)
+                results[model.get_name()][i]=evaluate_metric(y_pred,y_b,metric)
+            print(f"batch nr {i+1} finished")
+        return results
+    def measure_time(self,batch_nr:int,seed:int=42)->Dict[str, np.ndarray]:
+        results = self._init_results(batch_nr)
+        splits = self._init_splits(batch_nr,seed)
+        for i,split in enumerate(splits):
+            X_b=self.X[split]
+            y_b=self.y[split]
+            for model in self.models:
+                start_time = time.perf_counter()
+                model.predict(X_b)
+                end_time = time.perf_counter()
+                results[model.get_name()][i]=end_time-start_time
+            print(f"batch nr {i+1} finished")
+
+        return results
